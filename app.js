f7276527ab75e16f7f85b1878d967e7da7c5745e
Berliano <Berliano.535190078@stu.untar.ac.id>
2026-03-03 23:35:15 +0700

    Dashboard + Run scraper

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use("/manga", express.static("/data/manga"), {
 });
 
 const adminRoutes = require("./routes/admin");
-app.use("/api/admin", adminRoutes);
+//app.use("/api/admin", adminRoutes);
 
 // uploads manual
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
