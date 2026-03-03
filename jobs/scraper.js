45397127f133df632f55c3274f10f4848decd1c2
Berliano <Berliano.535190078@stu.untar.ac.id>
2026-03-04 04:43:25 +0700

    Fix Dashboard + Run scraper optimzed rata kiri semoga

--- jobs/scraper.js
+++ jobs/scraper.js
@@ -67,7 +67,7 @@ if (!fs.existsSync(mangaDir)) {
 async function initBrowser() {
   browser = await puppeteer.launch({
     headless: "new",
-    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+    executablePath: "/opt/render/.cache/puppeteer/chrome/linux-145.0.7632.77/chrome-linux64/chrome",
     args: [
       "--no-sandbox",
       "--disable-setuid-sandbox",
